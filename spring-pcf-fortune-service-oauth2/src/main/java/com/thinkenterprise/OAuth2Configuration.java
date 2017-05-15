0cd431164ed55216b60301c1c2c16ac5854645b1
schaefem <schaefem@MSGN04551.int.root.msg.ag>
2017-05-15 22:23:54 +0200

    First Commit

---
+++ spring-pcf-fortune-service-oauth2/src/main/java/com/thinkenterprise/OAuth2Configuration.java
@@ -0,0 +1,12 @@
+package com.thinkenterprise;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@Configuration
+@EnableResourceServer
+public class OAuth2Configuration {
+	
+	
+
+}
