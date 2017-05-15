3f9567ed8a6da6d0581202143057d79cb77c6f06
schaefem <schaefem@MSGN04551.int.root.msg.ag>
2017-05-15 22:25:42 +0200

    First Commit

---
+++ spring-pcf-oauth2-authorization-server/src/main/java/com/thinkenterprise/WebSecurityConfiguration.java
@@ -0,0 +1,63 @@
+package com.thinkenterprise;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Configuration of our web security 
+ * 
+ * 
+ * @author Michael Schäfer 
+ * 
+ * */
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter{
+	 
+	/**
+	 * Normally all request are secured. 
+	 * Customize the security constraints for our authorization server. 
+	 * Security is necessary for calling /oauth/authorize to get an authorization code. 
+	 * 
+	 *  
+	 * @author M. Schäfer 
+	 * 
+	 * */
+	
+	@Override
+     protected void configure(HttpSecurity http) throws Exception {
+
+		/*
+		http.httpBasic().
+			and().authorizeRequests().antMatchers("/oauth/authorize").permitAll().anyRequest().authenticated();
+*/
+		http.csrf().disable();
+		http.httpBasic().and().authorizeRequests().antMatchers("/oauth/authorize").authenticated().
+						 and().authorizeRequests().antMatchers("/oauth/token").authenticated().
+						 anyRequest().permitAll();
+     }
+
+	
+	/**
+	 * Crean Memory User with name and password and associated roles 
+	 * 
+	 * @author M. Schäfer 
+	 * 
+	 * */
+	
+     @Override
+     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+         
+    	 auth.inMemoryAuthentication()
+         .withUser("user")
+         .password("password")
+         .authorities("USER");
+     }
+
+}
